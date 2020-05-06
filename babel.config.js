cf77bdebb702fcf97dc2edb333441218864e1634
amogos <iulia.enes5@gmail.com>
2020-05-06 20:36:57 +0200

    make relay native work

--- babel.config.js
+++ babel.config.js
@@ -2,11 +2,11 @@ module.exports = api => {
   api.cache(true);
 
   return {
-    presets: ['module:metro-react-native-babel-preset'],
     plugins: [
-      'relay',
+      'babel-plugin-relay',
       '@babel/plugin-transform-runtime',
       // '@babel/plugin-proposal-class-properties', // Breaks fast refresh on native for some reason, left here if needed
     ].map(require.resolve),
+    presets: ['module:metro-react-native-babel-preset'],
   };
 };
