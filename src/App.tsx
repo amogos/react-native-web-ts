490c2eeafe9f52156e7b2792c87b224beddbf408
amogos <iulia.enes5@gmail.com>
2020-05-19 22:27:27 +0200

    wrap screens to inject environment without callback function

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,29 @@ import {createStackNavigator} from '@react-navigation/stack';
 import HomeScreen from './screens/HomeScreen';
 import AboutScreen from './screens/AboutScreen';
 import RelayEnv from './env/relay.env';
+import {Environment} from 'react-relay';
 
 const Stack = createStackNavigator();
+
+function withEnvironment(WrappedComponent: any) {
+  return class extends React.Component<any, any> {
+    public render() {
+      return <WrappedComponent environment={RelayEnv} />;
+    }
+  };
+}
 
 export default () => (
   <NavigationContainer>
     <Stack.Navigator initialRouteName="Home">
       <Stack.Screen
         name="Home"
-        component={() => <HomeScreen environment={RelayEnv} />}
+        component={withEnvironment(HomeScreen)}
       />
-      <Stack.Screen name="About" component={AboutScreen} />
+      <Stack.Screen
+        name="About"
+        component={withEnvironment(AboutScreen)}
+      />
     </Stack.Navigator>
   </NavigationContainer>
 );
