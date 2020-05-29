39b8826ab1c1c8cdd1dee6055494787e9782365e
amogos <iulia.enes5@gmail.com>
2020-05-29 17:49:54 +0200

    make slider render

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import {Button} from 'react-native-elements';
+import React, {useState} from 'react';
+import {StyleSheet, View} from 'react-native';
+import {Button, Image, Slider, Text} from 'react-native-elements';
 
 import AudioPlaylistController from '../audio-player/AudioPlaylistController';
 import {AudioPlayableItem} from '../audio-player/AudioPlayer';
@@ -16,15 +16,17 @@ const icon = require('../icons/home.jpg');
 const testPlaylist: AudioPlayableItem[] = [
   {
     title: 'test title',
-    artwork: 'none',
+    artwork:
+      'https://www.sailer-verlag.de/wp-content/uploads/2019/09/I-Love-English-Junior-Kinderzeitschrift-kategorie.jpg',
     id: 'id01',
-    url: require('../audio-player/assets/track.mp3'),
+    url: require('../audio-player/assets/sample-music.mp3'),
     artist: 'unknown',
   },
 ];
 
 export default (props: any) => {
   const {navigation, environment} = props;
+  const [value, setValue] = useState(0);
 
   const togglePlay = async () => {
     await AudioPlaylistController.addToPlaylist(...testPlaylist);
@@ -33,19 +35,46 @@ export default (props: any) => {
 
   return (
     <View style={styles.container}>
+      <Image
+        source={{
+          uri:
+            'https://www.sailer-verlag.de/wp-content/uploads/2019/09/I-Love-English-Junior-Kinderzeitschrift-kategorie.jpg',
+        }}
+        style={{width: 200, height: 200}}
+      />
+      <Slider
+        style={styles.slider}
+        maximumValue={100}
+        minimumValue={0}
+        minimumTrackTintColor="#307ecc"
+        maximumTrackTintColor="#000000"
+        value={50}
+        thumbStyle={styles.thumbstyle}
+        onValueChange={sliderValue => setValue(sliderValue)}
+      />
+      <Text>Value: {value}</Text>
       <Button
         title="play"
-        style={{width: 128, height: 128}}
         onPress={togglePlay}
+        style={{width: 200, height: 10}}
       />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  thumbstyle: {
+    backgroundColor:'#000000',
+    height:10
+  },
+  slider: {
+    backgroundColor: '#ecf0f1',
+    width: 300,
+  },
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#fcf0f1',
   },
 });
