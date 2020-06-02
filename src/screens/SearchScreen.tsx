dea4192b2cfadde817e3f41510a96b23ecac5654
amogos <iulia.enes5@gmail.com>
2020-06-02 16:59:44 +0200

    remove unused

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import {View, Text} from 'react-native';
-import {Image} from 'react-native-elements';
 
 export default ({navigation}: {navigation: any}) => {
   return (
